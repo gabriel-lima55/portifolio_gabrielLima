02bf5b8dd221b8099228facb534524c4bd213762
Gabriel Everton Alves de Lima <gabrielacademico55@gmail.com>
2026-02-05 21:41:20 -0300

    Fix case Contact component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,9 @@ export default function Header() {
   return (
     <header className="header">
       <div className="header-container">
+        <div className="logo">Gabriel Lima</div>
+
         <nav>
-          <a href="home">Gabriel Lima</a>
           <a href="#about">Sobre</a>
           <a href="#projects">Projetos</a>
           <a href="#experience">Experiência</a>
